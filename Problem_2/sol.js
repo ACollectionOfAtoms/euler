ea3883bd4bbc10add887950276cb5d31f64ae308
acollectionofatoms <hernandezdavid88@utexas.edu>
2015-11-11 19:29:03 -0600

    Javascript fib function without recurssion

---
+++ Problem_2/sol.js
@@ -0,0 +1,20 @@
+fib = function(n) {
+    var ans = 0;
+    sum = function(a) {
+        s = a.reduce(function(first, last) {
+            return first + last;
+            });
+        return s;
+    };
+    if (n === 0) {
+        return ans;
+    } else {
+        var last_two = [0, 1];
+        for (var i = 1; i < n; i ++) {
+            ans = sum(last_two);
+            last_two[0] = last_two[1];
+            last_two[1] = ans;
+        }
+    }
+    return ans;
+};
