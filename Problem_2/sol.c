a201203432744ba4f6bcd1acf7510f281045f88a
acollectionofatoms <hernandezdavid88@utexas.edu>
2015-11-12 19:22:15 -0600

    Learning to use valgrind in C...

--- Problem_2/sol.c
+++ Problem_2/sol.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
 
 int sum_array(int a[], int num_ele) {
-    int i, sum = 0;
-    for (i=0; i < num_ele; i ++) {
+    int sum = 0;
+    for (int i=0; i < num_ele; i ++) {
         sum = sum + a[i];
     }
     return sum;
 }
 
 int fib(int n) {
-    int ans = 0;
+    int i, ans = 0;
     if (n == 0){
-        return 0;
-    } else {
-        int last_two[2] = {0, 1};
-        for (int i; i < n; i++) {
+        return ans;
+    }else{
+        int last_two[] = {0, 1};
+        for (i; i < n; i++) {
             ans = sum_array(last_two, 2);
             last_two[0] = last_two[1];
             last_two[1] = ans;
@@ -23,7 +23,20 @@ int fib(int n) {
     return ans;
 }
 
-void main() {
-    int answer = fib(6);
-    printf("%i \n", answer);
+int main() {
+    int maximum = 4000000;
+    int evenSum = 0;
+    int n = 2;
+    while (n < 5) {
+        n++;
+        int var = fib(10);
+        printf("this is a number %i\n", var);
+        printf("what the %i\n", fib(n));
+        printf("%i\n", n);
+        if (fib(n) % 2 == 0) {
+            evenSum += fib(n);
+        }
+    }
+    printf("%i", evenSum);
+    return 0;
 }
